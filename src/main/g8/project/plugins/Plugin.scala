8dd7995fdf3406e6464e48d5f78394f7eef8dce5
Philip Cali <philip.cali@gmail.com>
2011-03-18 11:19:24 -0500

    Giter8 seemed to choke on the dashes, changed to underscore.

--- src/main/g8/project/plugins/Plugin.scala
+++ src/main/g8/project/plugins/Plugin.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class Plugin(info: ProjectInfo) extends PluginDefinition(info) {
-  val lwjglVersion = "com.github.philcali" % "sbt-lwjgl-plugin" % "$sbt-lwjgl-version$"
+  val lwjglVersion = "com.github.philcali" % "sbt-lwjgl-plugin" % "$sbt_lwjgl_version$"
 }
